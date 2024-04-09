6845de6fcf631725512134a50e63c9a33a78763d
RafaelCurado <rafael.curado@ua.pt>
2024-04-09 11:31:59 +0100

    update

--- README.md
+++ README.md
@@ -24,11 +24,38 @@ pip3 install -r requirements.txt
 ```
 ### Create database
 
-To run create/reset the database and its data, run the following command:
+Pull MySQL image from Docker Hub with:
+```bash
+$ docker pull mysql:latest
+```
+
+Create the container:
+```bash
+$ docker run --name bookings -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
+```
+
+Check if the container is running with:
+```bash
+$ docker ps 
+```
+
+To allocate the host ip address to the MySQL server instance use: 
 
 ```bash
-python3 database.py
+$ docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bookings 
 ```
+
+
+To create/reset the database and its data, run the following command:
+
+```bash
+python3 database.py <database_name> <password> --host <host_ip>
+```
+
+Host ip is optional, default is set to 172.17.0.2 
+
+
+
 
 ### Run the Application
 
--- database.py
+++ database.py
@@ -1,24 +1,29 @@
 import mysql.connector
+from argparse import ArgumentParser
 
-def reset_database():
+database = None
+
+def reset_database(name, pw, host="172.17.0.2"):
+    global database
+    database = {  # Store arguments in a global dictionary
+        'host': host,
+        'user': 'root',
+        'password': pw,
+        'database': name
+    }
+
     db = mysql.connector.connect(
-        host="172.17.0.2",
+        host=host,
         user="root",
-        password="password"
+        password=pw
     )
-
     cursor = db.cursor()
 
     # Drop existing database if it exists
-    cursor.execute("DROP DATABASE IF EXISTS testdb")
+    cursor.execute("DROP DATABASE IF EXISTS {}".format(name))
 
-    # Create new database
-    cursor.execute("CREATE DATABASE testdb")
-
-    # Use the new database
-    cursor.execute("USE testdb")
-
-    # Create table
+    cursor.execute("CREATE DATABASE {}".format(name))
+    cursor.execute("USE {}".format(name))
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS bookings (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -27,10 +32,17 @@ def reset_database():
         )
     """)
 
-    print("Database reset successfully")
+    print("Database '{}' reset successfully!".format(name))
 
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
-    reset_database()
+    parser = ArgumentParser(description="Reset a MySQL database.")
+    parser.add_argument("database_name", help="The name of the database to reset.")
+    parser.add_argument("password", help="The password for the database user.")
+    parser.add_argument("-H", "--host", help="The host address of the MySQL server (default: localhost)", default="172.17.0.2")
+    args = parser.parse_args()
+
+    reset_database(args.database_name, args.password, args.host)
